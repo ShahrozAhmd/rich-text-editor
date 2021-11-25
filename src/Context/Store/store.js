b9c84140495ec7c4d09dec11ce86cc10786aec65
Shahroz Ahmed <shahrozahmed286@gmail.com>
2021-11-25 12:21:24 +0500

    changes in context

--- src/Context/Store/store.js
+++ src/Context/Store/store.js
@@ -1,13 +1,15 @@
 import React, { createContext, useContext, useReducer } from "react";
 
-export const Store = createContext();
+export const Context = createContext();
 
-export const useStore = () => useContext(Store);
+export const Store = () => useContext(Context);
 
 const ContextProvider = ({ initialState, reducer, children }) => {
   const [state, dispatch] = useReducer(initialState, reducer);
 
-  return <Store.Provider value={[state, dispatch]}>{children}</Store.Provider>;
+  return (
+    <Context.Provider value={[state, dispatch]}>{children}</Context.Provider>
+  );
 };
 
 export default ContextProvider;
