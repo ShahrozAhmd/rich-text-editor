25cdc533815d9c1aa0df28bbb79c0a3e7d227eae
Shahroz Ahmed <shahrozahmed286@gmail.com>
2021-11-27 21:27:04 +0500

    setup material ui theme

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import  ContextProvider  from "Context/Store/store";
 import {state} from 'Context/State/state'
+import { ThemeProvider } from '@mui/material/styles';
+import {theme} from 'Theme/theme'
 
 ReactDOM.render(
   <React.StrictMode initialState={state} 
@@ -13,7 +15,9 @@ ReactDOM.render(
   >
     <ContextProvider>
       <Router>
+      <ThemeProvider  theme={theme}>
         <App />
+        </ThemeProvider>
       </Router>
     </ContextProvider>
   </React.StrictMode>,
